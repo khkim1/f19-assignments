c2ccd4e45f99c5c66e327dd3e8ad74c7263f27ac
John Kim <ilovekmla@gmail.com>
2019-10-29 11:19:42 -0700

    assign5 first push

--- lab1/starter/exercise2.ml
+++ lab1/starter/exercise2.ml
@@ -32,26 +32,44 @@ let main () =
   ] in
 
   let just_temp (l : forecast list) : float list =
-       raise Unimplemented
+    List.map ~f:(fun (x : forecast) : float -> x.temperature) l
   in
 
   assert (just_temp example_forecasts = [70.; 62.; 55.; 68.; 52.; 59.]);
 
   let average_temp (l : forecast list) : float =
-       raise Unimplemented
+    List.fold_left l ~init:0. ~f:(fun (accum : float) (n: forecast) : float ->
+                                    accum +. n.temperature /. (float (List.length l)))
   in
 
   assert (average_temp example_forecasts = 61.);
 
+  (* Rainy above 70% and humidity over 50% *)
   let humid_rainy_days (l : forecast list) : forecast list =
-       raise Unimplemented
+    let check_rain (weather: weather_type) : bool  =
+      match weather with
+      | Rainy v -> v >= 0.7
+      | _ -> false
+    in
+      List.filter ~f:(fun (y : forecast) : bool -> (check_rain y.weather) && y.humidity >= 0.5) l
   in
 
   assert (humid_rainy_days example_forecasts = [
     {weather = Rainy 0.7; temperature = 55.; humidity = 0.5}]);
 
   let weather_histogram (l : forecast list) : (weather_type * int) list =
-       raise Unimplemented
+    List.fold_left l ~init:[] ~f:(fun (accum : (weather_type * int) list) (x : forecast) : (weather_type * int) list ->
+      if List.length (List.filter accum ~f:(fun ((t, n) : weather_type * int) : bool ->
+        t = x.weather)) > 0 then
+        List.map accum ~f:(fun ((t, n) : (weather_type * int)) : (weather_type * int) ->
+          if t = x.weather then
+            (t, n + 1)
+          else
+            (t, n)
+        )
+      else
+        (x.weather, 1) :: accum
+    )
   in
 
   assert (weather_histogram example_forecasts = [
